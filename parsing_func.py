eae2d8a3fb090eae6031aaab98aca6f155068bde
Thomas Duffy <tommy.duffy@gmail.com>
2019-09-09 16:28:34 -0600

    modified python file class now produces dataframe with date time objets from json file

--- parsing_func.py
+++ parsing_func.py
@@ -4,6 +4,7 @@ import json
 import getpass
 import numpy as np
 import pandas as pd
+from datetime import datetime
 
 class coinmarketcap_jsonfile(object):
 
@@ -22,14 +23,28 @@ class coinmarketcap_jsonfile(object):
             print(value)
 
     def key_value_to_pd(self,key,value):
-        ''' This will take the values and turn this into a pandas dataframe'''
+        ''' This will take the values and turn this into a pandas dataframe
+        Input
+        -------
+        key - The key of the dictionary being passed in
+        value- The values associated with that key
+        Return
+        -------
+        df - A pandas dataframe corresponding to those with time split out'''
+
         lst1,lst2 = zip(*value)
         return pd.DataFrame.from_dict({"time":lst1,f"{key}_value":lst2})
 
-    def merge_pd(self):
-        ''' This func
-        this by if it is the first iteration, to instantiate the data frame,
-         then every iteration after that, it will merge it on time'''
+    def merge_df_on_time(self):
+        ''' This function takes the values and keys associated with the json file
+        and returns a dataframe joined on the time column,
+        it does this by if it is the first iteration in the for loop it will instantiate the data frame,
+        then every iteration after that, it will merge it on time with the other values.
+
+         Return
+         -------
+         df - a dataframe of all the values joined on the time metric.'''
+
         i=0
         for key,value in self.data.items():
             
@@ -39,7 +54,13 @@ class coinmarketcap_jsonfile(object):
             else:
                 df=pd.merge(df,self.key_value_to_pd(key,value), on='time',how='left')
         return df
-    def parse_time_data(self,df)
+
+    def parse_time_data(self):
+        ''' This will take in the dataframe created by mege_df_on_time and reformate the time
+        column into a date time objects.'''
+        df = self.merge_df_on_time()
+        df['time'] = df['time'].apply(lambda x :datetime.fromtimestamp(int(int(x)/1000)))
+        return df
 
 
 if __name__ == "__main__":
